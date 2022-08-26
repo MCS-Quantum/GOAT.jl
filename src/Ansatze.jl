4da1bc92c9388865784c2f75fd6738dd43f9892d
Paul Kairys <pkairys@anl.gov>
2022-08-26 12:59:30 -0500

    fixing poly ansatz

--- src/Ansatze.jl
+++ src/Ansatze.jl
@@ -278,7 +278,7 @@ function poly_coefficient(p,t,i,K::Int64,N::Int64)
     c = 0.0
     for n in 1:N
         j = (i-1)*K*N + (n-1)*K # Get the linear index of the ith's control term's nth basis function
-        c += p[j+1]*(t-p[j+1])^(n-1)
+        c += p[j+1]*(t-p[j+2])^(n-1)
     end
     return c
 end
@@ -297,7 +297,7 @@ function ∂poly_coefficient(p,t,i,l,K::Int64,N::Int64)
             if j+1 == l
                 c += (t-p[j+1])^n
             elseif j+2 == l
-                c += -p[j+1]*(n-1)*(t-p[j+1])^(n-2)
+                c += -p[j+1]*(n-1)*(t-p[j+2])^(n-2)
             end
         end
         return c
