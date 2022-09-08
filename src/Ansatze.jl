c6618aa523229f1c26c07c3940ad6f5c6a7ed1ae
Paul Kairys <pkairys@anl.gov>
2022-09-08 14:36:38 -0500

    cleaning up Ansatze library

--- src/Ansatze.jl
+++ src/Ansatze.jl
@@ -74,142 +74,9 @@ end
 
 ## Ansatze
 
-function constant_ansatz(t,p,k)
-    a = p.params
-    imap = p.map
-    Ns = p.Ns
-    @assert length(Ns)==1
-    c = a[imap[k,1,1]]
-    return c
-end
-
-function derivative_constant_ansatz(t,p,k,n,q)
-    # If the term in the Hamiltonian is  H_k = c_k*A then ∂/∂c_k H_k = A
-    return 1
-end
-
-
-function fourier_ansatz(t,p,k)
-    a = p.params
-    imap = p.map
-    Ns = p.Ns
+function fourier_coefficient(p,t,i,N::Int64)
     c = 0.0
-    # For the channel k the coefficient is defined by
-    for n in 1:Ns[k]
-        c += a[imap[k,n,1]]*sin(a[imap[k,n,2]]*t+a[imap[k,n,3]])
-    end
-    return c
-end
-
-function derivative_fourier_ansatz(t,p,k,n,q)
-    a = p.params
-    imap = p.map
-    if q==1
-        c = sin(a[imap[k,n,2]]*t+a[imap[k,n,3]])
-        return c
-    elseif q==2
-        c = a[imap[k,n,1]]*t*cos(a[imap[k,n,2]]*t+a[imap[k,n,3]])
-        return c
-    else
-        c = a[imap[k,n,1]]*cos(a[imap[k,n,2]]*t+a[imap[k,n,3]])
-        return c
-    end
-end
-
-function carrier_fourier_ansatz(t,p,k)
-    a = p.params
-    imap = p.map
-    Ns = p.Ns
-    c = 0.0
-    # For the channel k the coefficient is defined by
-    for n in 1:Ns[k]
-        c += (a[imap[k,n,1]]*sin(a[imap[k,n,2]]*t+a[imap[k,n,3]]))*cos(a[imap[k,n,4]]*t)
-    end
-    return c
-end
-
-function derivative_carrier_fourier_ansatz(t,p,k,n,q)
-    a = p.params
-    imap = p.map
-    if q==1
-        c = sin(a[imap[k,n,2]]*t+a[imap[k,n,3]])*cos(a[imap[k,n,4]]*t)
-        return c
-    elseif q==2
-        c = a[imap[k,n,1]]*t*cos(a[imap[k,n,2]]*t+a[imap[k,n,3]])*cos(a[imap[k,n,4]]*t)
-        return c
-    elseif q==3
-        c = a[imap[k,n,1]]*cos(a[imap[k,n,2]]*t+a[imap[k,n,3]])*cos(a[imap[k,n,4]]*t)
-        return c
-    else
-        c = -t*sin(a[imap[k,n,4]]*t)*(a[imap[k,n,1]]*sin(a[imap[k,n,2]]*t+a[imap[k,n,3]]))
-    end
-end
-
-
-function gaussian_ansatz(t,p,k)
-    a = p.params
-    imap = p.map
-    Ns = p.Ns
-    c = 0.0
-    # For the channel k the coefficient is defined by
-    for n in 1:Ns[k]
-        c += a[imap[k,n,1]]*exp(-0.5*( (t-a[imap[k,n,2]]) / a[imap[k,n,3]])^2)
-    end
-    return c
-end
-
-function derivative_gaussian_ansatz(t,p,k,n,q)
-    a = p.params
-    imap = p.map
-    if q==1
-        c = exp(-0.5*( (t-a[imap[k,n,2]]) / a[imap[k,n,3]])^2)
-        return c
-    elseif q==2
-        c = a[imap[k,n,1]]*(t-a[imap[k,n,2]])*exp(-0.5*( (t-a[imap[k,n,2]]) / a[imap[k,n,3]])^2) /(a[imap[k,n,3]]^2)
-        return c
-    else
-        c = a[imap[k,n,1]]*((t-a[imap[k,n,2]])^2)*exp(-0.5*( (t-a[imap[k,n,2]]) / a[imap[k,n,3]])^2)/(a[imap[k,n,3]]^3)
-        return c
-    end
-end
-
-
-function morlet_ansatz(t,p,k)
-    a = p.params
-    imap = p.map
-    N = p.Ns[k]
-    c = 0.0
-    # For the channel k the coefficient is defined by
-    for n in 1:N
-        c += a[imap[k,n,1]]*exp(-0.5*( (t-a[imap[k,n,2]]) / a[imap[k,n,3]])^2)*sin(a[imap[k,n,4]]*t+a[imap[k,n,5]])
-    end
-    return c
-end
-
-function derivative_morlet_ansatz(t,p,k,n,q)
-    a = p.params
-    imap = p.map
-    if q==1
-        c = exp(-0.5*( (t-a[imap[k,n,2]]) / a[imap[k,n,3]])^2)*sin(a[imap[k,n,4]]*t+a[imap[k,n,5]])
-        return c
-    elseif q==2
-        c = a[imap[k,n,1]]*sin(a[imap[k,n,4]]*t+a[imap[k,n,5]])*(t-a[imap[k,n,2]])*exp(-0.5*( (t-a[imap[k,n,2]]) / a[imap[k,n,3]])^2) /(a[imap[k,n,3]]^2)
-        return c
-    elseif q==3
-        c = a[imap[k,n,1]]*sin(a[imap[k,n,4]]*t+a[imap[k,n,5]])*((t-a[imap[k,n,2]])^2)*exp(-0.5*( (t-a[imap[k,n,2]]) / a[imap[k,n,3]])^2)/(a[imap[k,n,3]]^3)
-        return c
-    elseif q==4
-        c = a[imap[k,n,1]]*exp(-0.5*( (t-a[imap[k,n,2]]) / a[imap[k,n,3]])^2)*t*cos(a[imap[k,n,4]]*t+a[imap[k,n,5]])
-        return c
-    else
-        c = a[imap[k,n,1]]*exp(-0.5*( (t-a[imap[k,n,2]]) / a[imap[k,n,3]])^2)*cos(a[imap[k,n,4]]*t+a[imap[k,n,5]])
-        return c
-    end
-end
-
-
-function fourier_coefficient(p,t,i,K::Int64,N::Int64)
-    c = 0.0
+    K = 3
     for n in 1:N
         j = (i-1)*K*N + (n-1)*K # Get the linear index of the ith's control term's nth basis function
         c += p[j+1]*cos(p[j+2]*t+p[j+3])
@@ -221,7 +88,8 @@ end
 """
 Partial deriv of c_func w.r.t the lth element of p
 """
-function ∂fourier_coefficient(p,t,i,l,K::Int64,N::Int64)
+function ∂fourier_coefficient(p,t,i,l,N::Int64)
+    K = 3
     jmin = (i-1)*K*N+1
     jmax = i*K*N
     if l >= jmin && l <= jmax # Linear indices between the ith control term and the i+1th control terms
@@ -241,8 +109,9 @@ function ∂fourier_coefficient(p,t,i,l,K::Int64,N::Int64)
     return 0.0
 end
 
-function gaussian_coefficient(p,t,i,K::Int64,N::Int64)
+function gaussian_coefficient(p,t,i,N::Int64)
     c = 0.0
+    K = 3
     for n in 1:N
         j = (i-1)*K*N + (n-1)*K # Get the linear index of the ith's control term's nth basis function
         c += p[j+1]*exp(-0.5*( (t-p[j+2]) / p[j+3])^2)
@@ -254,7 +123,8 @@ end
 """
 Partial deriv of c_func w.r.t the lth element of p
 """
-function ∂gaussian_coefficient(p,t,i,l,K::Int64,N::Int64)
+function ∂gaussian_coefficient(p,t,i,l,N::Int64)
+    K = 3
     jmin = (i-1)*K*N+1
     jmax = i*K*N
     if l >= jmin && l <= jmax # Linear indices between the ith control term and the i+1th control terms
