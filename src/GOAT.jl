8bd7b432151ec1dbae3ee43b57961593e3c453bd
Paul Kairys <pkairys@anl.gov>
2023-04-26 11:22:16 -0500

    reorganizing structure definitions ordering

--- src/GOAT.jl
+++ src/GOAT.jl
@@ -27,9 +27,10 @@ module GOAT
 
 using OrdinaryDiffEq, SparseArrays, LinearAlgebra, Distributed, Optim
 
-export SE_action, GOAT_action, ControllableSystem, ControllableSystem, make_SE_update_function
+export SE_action, GOAT_action, ControllableSystem, make_SE_update_function
 export make_GOAT_update_function, solve_SE, solve_GOAT_eoms, make_GOAT_initial_state
 export QOCProblem, GOAT_infidelity_reduce_map, SE_infidelity_reduce_map
+export QOCParameters
 export solve_GOAT_eoms_reduce, parallel_GOAT_fg!, find_optimal_controls, evaluate_infidelity
 
 
@@ -44,6 +45,203 @@ export fourier_coefficient, ∂fourier_coefficient, ∂gaussian_coefficient, gau
 include("Utilities.jl")
 export get_sinusoidal_coefficients_from_FFT, truncated_inv_fft
 export colored_noise, time_domain_signal, test_derivatives
+
+struct ControllableSystem{A,B,C,D,E}
+    d_ms::A
+    d_ls::A
+    d_vs::B
+    c_ls::Vector{Vector{Int64}}
+    c_ms::Vector{Vector{Int64}}
+    c_vs::Vector{Vector{ComplexF64}}
+    coefficient_func::C
+    ∂coefficient_func::D
+    reference_frame_generator::E
+    reference_frame_storage::E
+    use_rotating_frame::Bool
+    dim::Int64
+    orthogonal_basis::Bool
+    tol::Float64
+end
+
+
+function ControllableSystem(drift_op, basis_ops, c_func, ∂c_func)
+    d_ls,d_ms,d_vs = findnz(drift_op)
+    d = size(drift_op,1)
+    c_ls = [findnz(op)[1] for op in basis_ops]
+    c_ms = [findnz(op)[2] for op in basis_ops]
+    c_vs = [findnz(op)[3] for op in basis_ops]
+    return ControllableSystem{typeof(d_ls), typeof(d_vs), typeof(c_func), typeof(∂c_func), Nothing}(d_ms, d_ls, d_vs, c_ls, c_ms, c_vs, c_func, ∂c_func, nothing, nothing, false, d, false, 0.0)
+    
+end
+
+function ControllableSystem(drift_op, basis_ops, c_func)
+    ∂c_func(t) = 0
+    d_ls,d_ms,d_vs = findnz(drift_op)
+    d = size(drift_op,1)
+    c_ls = [findnz(op)[1] for op in basis_ops]
+    c_ms = [findnz(op)[2] for op in basis_ops]
+    c_vs = [findnz(op)[3] for op in basis_ops]
+    return ControllableSystem{typeof(d_ls), typeof(d_vs), typeof(c_func), typeof(∂c_func), Nothing}(d_ms, d_ls, d_vs, c_ls, c_ms, c_vs, c_func, ∂c_func, nothing, nothing, false, d, false, 0.0)
+    
+end
+
+function ControllableSystem(drift_op, basis_ops, RF_generator::Eigen, c_func, ∂c_func; sparse_tol = 1e-12)
+    d = size(drift_op,1)
+    F = RF_generator
+    N = size(basis_ops,1)
+    c_ls = []
+    c_ms = []
+    c_vs = []
+    as = F.values
+    a_diffs = zeros(ComplexF64,d,d)
+    aj_drift_aks = zeros(ComplexF64,d,d)
+    aj_hi_aks = zeros(ComplexF64,d,N,d)
+    for j in 1:d
+        for k in 1:d
+            aj = F.values[j]
+            ak = F.values[k]
+            a_diffs[j,k] = aj-ak
+            aj_vec = @view F.vectors[:,j]
+            ak_vec = @view F.vectors[:,k]
+            aj_drift_ak = adjoint(aj_vec)*drift_op*ak_vec
+            aj_drift_aks[j,k] = aj_drift_ak
+
+            new_basis_op = sparse(aj_vec*adjoint(ak_vec))
+            droptol!(new_basis_op,sparse_tol)
+            for i in 1:N
+                aj_hi_ak = adjoint(aj_vec)*basis_ops[i]*ak_vec
+                aj_hi_aks[j,i,k] = aj_hi_ak
+            end
+
+            ls,ms,vs = findnz(new_basis_op)
+            push!(c_ls,ls)
+            push!(c_ms, ms)
+            push!(c_vs,vs)
+        end
+    end
+    
+    function new_c_func(p,t,j,k)
+        c = 0.0+0.0im
+        diag_term = 0.0+0.0im
+        if j==k
+            diag_term = as[j]
+        end
+        for i in 1:N
+            c += c_func(p,t,i)*aj_hi_aks[j,i,k]
+        end
+        adiff = a_diffs[j,k]
+        aj_drift_ak = aj_drift_aks[j,k]
+        return cis(t*adiff)*(aj_drift_ak+c+diag_term)
+    end
+
+    function new_∂c_func(p,t,j,k,m)
+        c = 0.0+0.0im
+        for i in 1:N
+            c += ∂c_func(p,t,i,m)*aj_hi_aks[j,i,k]
+        end
+        adiff = a_diffs[j,k]
+        return cis(t*adiff)*c
+    end
+
+    return ControllableSystem{Nothing, Nothing, typeof(new_c_func), typeof(new_∂c_func), Nothing}(nothing, nothing, nothing, c_ls,c_ms,c_vs,new_c_func,new_∂c_func, nothing, nothing, false, d, true, sparse_tol)
+end
+
+
+function ControllableSystem(drift_op, basis_ops, RF_generator::Matrix, c_func, ∂c_func; sparse_tol = 1e-12)
+    d = size(drift_op,1)
+    F = eigen(RF_generator)
+    N = size(basis_ops,1)
+    c_ls = []
+    c_ms = []
+    c_vs = []
+    as = F.values
+    a_diffs = zeros(ComplexF64,d,d)
+    aj_drift_aks = zeros(ComplexF64,d,d)
+    aj_hi_aks = zeros(ComplexF64,d,N,d)
+    for j in 1:d
+        for k in 1:d
+            aj = F.values[j]
+            ak = F.values[k]
+            a_diffs[j,k] = aj-ak
+            aj_vec = @view F.vectors[:,j]
+            ak_vec = @view F.vectors[:,k]
+            aj_drift_ak = adjoint(aj_vec)*drift_op*ak_vec
+            aj_drift_aks[j,k] = aj_drift_ak
+
+            new_basis_op = sparse(aj_vec*adjoint(ak_vec))
+            droptol!(new_basis_op,sparse_tol)
+            for i in 1:N
+                aj_hi_ak = adjoint(aj_vec)*basis_ops[i]*ak_vec
+                aj_hi_aks[j,i,k] = aj_hi_ak
+            end
+
+            ls,ms,vs = findnz(new_basis_op)
+            push!(c_ls,ls)
+            push!(c_ms, ms)
+            push!(c_vs,vs)
+        end
+    end
+    
+    function new_c_func(p,t,j,k)
+        c = 0.0+0.0im
+        diag_term = 0.0+0.0im
+        if j==k
+            diag_term = as[j]
+        end
+        for i in 1:N
+            c += c_func(p,t,i)*aj_hi_aks[j,i,k]
+        end
+        adiff = a_diffs[j,k]
+        aj_drift_ak = aj_drift_aks[j,k]
+        return cis(t*adiff)*(aj_drift_ak+c+diag_term)
+    end
+
+    function new_∂c_func(p,t,j,k,m)
+        c = 0.0+0.0im
+        for i in 1:N
+            c += ∂c_func(p,t,i,m)*aj_hi_aks[j,i,k]
+        end
+        adiff = a_diffs[j,k]
+        return cis(t*adiff)*c
+    end
+
+    return ControllableSystem{Nothing, Nothing, typeof(new_c_func), typeof(new_∂c_func), Nothing}(nothing, nothing, nothing, c_ls,c_ms,c_vs,new_c_func,new_∂c_func, nothing, nothing, false, d, true, sparse_tol)
+end
+
+
+function ControllableSystem(drift_op, basis_ops, RF_generator::LinearAlgebra.Diagonal, c_func, ∂c_func)
+    d_ls,d_ms,d_vs = findnz(drift_op)
+    d = size(drift_op,1)
+    c_ls = [findnz(op)[1] for op in basis_ops]
+    c_ms = [findnz(op)[2] for op in basis_ops]
+    c_vs = [findnz(op)[3] for op in basis_ops]
+    return ControllableSystem{typeof(d_ls), typeof(d_vs) ,typeof(c_func),typeof(∂c_func),typeof(RF_generator)}(d_ms, d_ls, d_vs, c_ls, c_ms, c_vs, c_func, ∂c_func, RF_generator, similar(RF_generator), true , d, false, 0.0)
+end
+
+struct QOCProblem
+    Pc::Array{ComplexF64}
+    Pc_dim::Int64
+    Pa::Array{ComplexF64}
+    Pa_dim::Int64
+    target::Array{ComplexF64}
+    control_time::Float64
+end
+
+QOCProblem(target, control_time, Pc, Pa) = QOCProblem(Pc, Int(round(tr(Pc))), Pa, Int(round(tr(Pa))), target, control_time)
+
+struct QOCParameters{A,B,C,D,E}
+    ODE_options::Dict
+    SE_reduce_map::A
+    GOAT_reduce_map::B
+    optim_alg::C
+    optim_options::D
+    num_params_per_GOAT::E
+end
+
+function QOCParameters(ODE_options,SE_reduce_map,GOAT_reduce_map,optim_alg,optim_options; num_params_per_GOAT=nothing)
+    return QOCParameters{typeof(SE_reduce_map),typeof(GOAT_reduce_map),typeof(optim_alg),typeof(optim_options),typeof(num_params_per_GOAT)}(ODE_options,SE_reduce_map,GOAT_reduce_map,optim_alg,optim_options,num_params_per_GOAT)
+end
+
 
 function SE_action(du::Array{ComplexF64},u::Array{ComplexF64},p::Vector{Float64},t::Float64,c_ms::Vector{Vector{Int64}},c_ls::Vector{Vector{Int64}},c_vs::Vector{Vector{ComplexF64}},c_func::Function)
     d = size(u,2) # Dimension of unitary/Hamiltonian
@@ -311,179 +509,6 @@ function GOAT_action(du::Array{ComplexF64},u::Array{ComplexF64},p::Vector{Float6
     lmul!(-im, du)
 end
 
-struct ControllableSystem{A,B,C,D,E}
-    d_ms::A
-    d_ls::A
-    d_vs::B
-    c_ls::Vector{Vector{Int64}}
-    c_ms::Vector{Vector{Int64}}
-    c_vs::Vector{Vector{ComplexF64}}
-    coefficient_func::C
-    ∂coefficient_func::D
-    reference_frame_generator::E
-    reference_frame_storage::E
-    use_rotating_frame::Bool
-    dim::Int64
-    orthogonal_basis::Bool
-    tol::Float64
-end
-
-
-function ControllableSystem(drift_op, basis_ops, c_func, ∂c_func)
-    d_ls,d_ms,d_vs = findnz(drift_op)
-    d = size(drift_op,1)
-    c_ls = [findnz(op)[1] for op in basis_ops]
-    c_ms = [findnz(op)[2] for op in basis_ops]
-    c_vs = [findnz(op)[3] for op in basis_ops]
-    return ControllableSystem{typeof(d_ls), typeof(d_vs), typeof(c_func), typeof(∂c_func), Nothing}(d_ms, d_ls, d_vs, c_ls, c_ms, c_vs, c_func, ∂c_func, nothing, nothing, false, d, false, 0.0)
-    
-end
-
-function ControllableSystem(drift_op, basis_ops, c_func)
-    ∂c_func(t) = 0
-    d_ls,d_ms,d_vs = findnz(drift_op)
-    d = size(drift_op,1)
-    c_ls = [findnz(op)[1] for op in basis_ops]
-    c_ms = [findnz(op)[2] for op in basis_ops]
-    c_vs = [findnz(op)[3] for op in basis_ops]
-    return ControllableSystem{typeof(d_ls), typeof(d_vs), typeof(c_func), typeof(∂c_func), Nothing}(d_ms, d_ls, d_vs, c_ls, c_ms, c_vs, c_func, ∂c_func, nothing, nothing, false, d, false, 0.0)
-    
-end
-
-function ControllableSystem(drift_op, basis_ops, RF_generator::Eigen, c_func, ∂c_func; sparse_tol = 1e-12)
-    d = size(drift_op,1)
-    F = RF_generator
-    N = size(basis_ops,1)
-    c_ls = []
-    c_ms = []
-    c_vs = []
-    as = F.values
-    a_diffs = zeros(ComplexF64,d,d)
-    aj_drift_aks = zeros(ComplexF64,d,d)
-    aj_hi_aks = zeros(ComplexF64,d,N,d)
-    for j in 1:d
-        for k in 1:d
-            aj = F.values[j]
-            ak = F.values[k]
-            a_diffs[j,k] = aj-ak
-            aj_vec = @view F.vectors[:,j]
-            ak_vec = @view F.vectors[:,k]
-            aj_drift_ak = adjoint(aj_vec)*drift_op*ak_vec
-            aj_drift_aks[j,k] = aj_drift_ak
-
-            new_basis_op = sparse(aj_vec*adjoint(ak_vec))
-            droptol!(new_basis_op,sparse_tol)
-            for i in 1:N
-                aj_hi_ak = adjoint(aj_vec)*basis_ops[i]*ak_vec
-                aj_hi_aks[j,i,k] = aj_hi_ak
-            end
-
-            ls,ms,vs = findnz(new_basis_op)
-            push!(c_ls,ls)
-            push!(c_ms, ms)
-            push!(c_vs,vs)
-        end
-    end
-    
-    function new_c_func(p,t,j,k)
-        c = 0.0+0.0im
-        diag_term = 0.0+0.0im
-        if j==k
-            diag_term = as[j]
-        end
-        for i in 1:N
-            c += c_func(p,t,i)*aj_hi_aks[j,i,k]
-        end
-        adiff = a_diffs[j,k]
-        aj_drift_ak = aj_drift_aks[j,k]
-        return cis(t*adiff)*(aj_drift_ak+c+diag_term)
-    end
-
-    function new_∂c_func(p,t,j,k,m)
-        c = 0.0+0.0im
-        for i in 1:N
-            c += ∂c_func(p,t,i,m)*aj_hi_aks[j,i,k]
-        end
-        adiff = a_diffs[j,k]
-        return cis(t*adiff)*c
-    end
-
-    return ControllableSystem{Nothing, Nothing, typeof(new_c_func), typeof(new_∂c_func), Nothing}(nothing, nothing, nothing, c_ls,c_ms,c_vs,new_c_func,new_∂c_func, nothing, nothing, false, d, true, sparse_tol)
-end
-
-
-function ControllableSystem(drift_op, basis_ops, RF_generator::Matrix, c_func, ∂c_func; sparse_tol = 1e-12)
-    d = size(drift_op,1)
-    F = eigen(RF_generator)
-    N = size(basis_ops,1)
-    c_ls = []
-    c_ms = []
-    c_vs = []
-    as = F.values
-    a_diffs = zeros(ComplexF64,d,d)
-    aj_drift_aks = zeros(ComplexF64,d,d)
-    aj_hi_aks = zeros(ComplexF64,d,N,d)
-    for j in 1:d
-        for k in 1:d
-            aj = F.values[j]
-            ak = F.values[k]
-            a_diffs[j,k] = aj-ak
-            aj_vec = @view F.vectors[:,j]
-            ak_vec = @view F.vectors[:,k]
-            aj_drift_ak = adjoint(aj_vec)*drift_op*ak_vec
-            aj_drift_aks[j,k] = aj_drift_ak
-
-            new_basis_op = sparse(aj_vec*adjoint(ak_vec))
-            droptol!(new_basis_op,sparse_tol)
-            for i in 1:N
-                aj_hi_ak = adjoint(aj_vec)*basis_ops[i]*ak_vec
-                aj_hi_aks[j,i,k] = aj_hi_ak
-            end
-
-            ls,ms,vs = findnz(new_basis_op)
-            push!(c_ls,ls)
-            push!(c_ms, ms)
-            push!(c_vs,vs)
-        end
-    end
-    
-    function new_c_func(p,t,j,k)
-        c = 0.0+0.0im
-        diag_term = 0.0+0.0im
-        if j==k
-            diag_term = as[j]
-        end
-        for i in 1:N
-            c += c_func(p,t,i)*aj_hi_aks[j,i,k]
-        end
-        adiff = a_diffs[j,k]
-        aj_drift_ak = aj_drift_aks[j,k]
-        return cis(t*adiff)*(aj_drift_ak+c+diag_term)
-    end
-
-    function new_∂c_func(p,t,j,k,m)
-        c = 0.0+0.0im
-        for i in 1:N
-            c += ∂c_func(p,t,i,m)*aj_hi_aks[j,i,k]
-        end
-        adiff = a_diffs[j,k]
-        return cis(t*adiff)*c
-    end
-
-    return ControllableSystem{Nothing, Nothing, typeof(new_c_func), typeof(new_∂c_func), Nothing}(nothing, nothing, nothing, c_ls,c_ms,c_vs,new_c_func,new_∂c_func, nothing, nothing, false, d, true, sparse_tol)
-end
-
-
-function ControllableSystem(drift_op, basis_ops, RF_generator::LinearAlgebra.Diagonal, c_func, ∂c_func)
-    d_ls,d_ms,d_vs = findnz(drift_op)
-    d = size(drift_op,1)
-    c_ls = [findnz(op)[1] for op in basis_ops]
-    c_ms = [findnz(op)[2] for op in basis_ops]
-    c_vs = [findnz(op)[3] for op in basis_ops]
-    return ControllableSystem{typeof(d_ls), typeof(d_vs) ,typeof(c_func),typeof(∂c_func),typeof(RF_generator)}(d_ms, d_ls, d_vs, c_ls, c_ms, c_vs, c_func, ∂c_func, RF_generator, similar(RF_generator), true , d, false, 0.0)
-end
-
-
 function make_SE_update_function(sys::ControllableSystem)
     if sys.d_ls === nothing
         if sys.orthogonal_basis
@@ -539,17 +564,6 @@ function solve_GOAT_eoms(sys::ControllableSystem, opt_param_inds::Vector{Int}, T
     sol = solve(prob; args...)
     return sol
 end
-
-struct QOCProblem
-    Pc::Array{ComplexF64}
-    Pc_dim::Int64
-    Pa::Array{ComplexF64}
-    Pa_dim::Int64
-    target::Array{ComplexF64}
-    control_time::Float64
-end
-
-QOCProblem(target, control_time, Pc, Pa) = QOCProblem(Pc, Int(round(tr(Pc))), Pa, Int(round(tr(Pa))), target, control_time)
 
 function GOAT_infidelity_reduce_map(sys::ControllableSystem, prob::QOCProblem, goat_sol)
     d = sys.dim    
@@ -652,30 +666,17 @@ function parallel_GOAT_fg!(F, G, x, p_storage, opt_param_inds, sys::Controllable
 
 end
 
-struct QOCParameters{A,B,C,D,E}
-    ODE_options::Dict
-    SE_reduce_map::A
-    GOAT_reduce_map::B
-    optim_alg::C
-    optim_options::D
-    num_params_per_GOAT::E
-end
-
-function QOCParameters(ODE_options,SE_reduce_map,GOAT_reduce_map,optim_alg,optim_options; num_params_per_GOAT=nothing)
-    return QOCParameters{typeof(SE_reduce_map),typeof(GOAT_reduce_map),typeof(optim_alg),typeof(optim_options),typeof(num_params_per_GOAT)}(ODE_options,SE_reduce_map,GOAT_reduce_map,optim_alg,optim_options,num_params_per_GOAT)
-end
-
 function find_optimal_controls(p0, sys::ControllableSystem, prob::QOCProblem, params::QOCParameters)
     fg!(F,G,x) = parallel_GOAT_fg!(F, G, x, sys, prob, params)
     res = Optim.optimize(Optim.only_fg!(fg!), p0, params.optim_alg, params.optim_options)
     return res
 end
 
-function find_optimal_controls(p0, opt_param_inds, sys::ControllableSystem, prob::QOCProblem, params::QOCParameters, SE_reduce_map, GOAT_reduce_map, ODE_options, optim_alg, optim_options ; num_params_per_GOAT=nothing)
+function find_optimal_controls(p0, opt_param_inds, sys::ControllableSystem, prob::QOCProblem, params::QOCParameters)
     p_storage = deepcopy(p0)
     fg!(F,G,x) = parallel_GOAT_fg!(F, G, x, p_storage, opt_param_inds, sys, prob, params)
     x0 = p0[opt_param_inds]
-    res = Optim.optimize(Optim.only_fg!(fg!), x0, optim_alg, optim_options)
+    res = Optim.optimize(Optim.only_fg!(fg!), x0, params.optim_alg, params.optim_options)
     return res
 end
 
