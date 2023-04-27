f138cb31dc1ce6be096296e2da50fa6bb17df6c5
Paul Kairys <pkairys@anl.gov>
2023-04-27 11:27:01 -0500

    adding deploydocs to docs/src/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -14,6 +14,10 @@ makedocs(
     ]
 )
 
+deploydocs(
+    repo = "github.com/MCS-Quantum/GOAT.jl.git",
+)
+
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
