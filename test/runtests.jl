a0474873ce9ee7c79873eb55f1819d4ec78c2230
Paul Kairys <pkairys@anl.gov>
2023-04-28 09:57:25 -0500

    adding test of fourier ansatz and decomposition functions

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
 using GOAT
 
 
-a = 2
-b = 3
-@assert a<b
+ts = [0:0.00001:100;];
+tau = 1.2352
+N = 3
+p0 = rand(N*3)
+s(t) = fourier_coefficient(p0,t,1,N)
+signal = s.(ts)
+as, fs, phis = get_sinusoidal_basis_parameters(ts,signal)
+s_fft(t) = time_domain_signal(t,as,fs,phis)
+@assert isapprox(s(tau),s_fft(tau);atol=1e-5)
+p = [y for x in zip(as,fs,phis) for y in x]
+s_ansatz(t) = fourier_coefficient(p,t,1,length(as))
+@assert isapprox(s(tau),s_ansatz(tau);atol=1e-5)
+
+
